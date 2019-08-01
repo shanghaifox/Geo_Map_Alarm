45d0c14b53533f17360246c6215242cd86553fd9
Jun Yang <Jun.Yang@us.gt.com>
2019-08-01 14:01:50 -0400

    Commit in the project.

---
+++ app/src/main/java/com/example/geo_map_alarm/MapsActivity.java
@@ -0,0 +1,83 @@
+package com.example.geo_map_alarm;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //GPS Tracker
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    GPSTracker gps;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        //check GPS Permission
+        try {
+            if (ContextCompat.checkSelfPermission(this, mPermission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{mPermission}, REQUEST_CODE_PERMISSION);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //GPS Tracker
+        // create class object
+        gps = new GPSTracker(MapsActivity.this);
+        double latitude = 0;
+        double longitude = 0;
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+            // \n is for new line
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
+                    + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+
+        //Google Map
+        mMap = googleMap;
+        // Add a marker in current Location and move the camera
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng current_Location = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(current_Location).title("Current Location"));
+
+        LatLng target_Location = gps.getLocationFromAddress(this, "750 3rd Ave,New York");
+        mMap.addMarker(new MarkerOptions().position(target_Location).title("Target Location"));
+
+        double distance = gps.distanceBetween(current_Location, target_Location);
+        Toast.makeText(getApplicationContext(), "distance is" + distance, Toast.LENGTH_LONG).show();
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current_Location, 3));
+    }
+}
